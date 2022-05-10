d1ffefa52e5a208f84b8800dbd693a4f27866b95
iledarn <iledarn@gmail.com>
2022-05-10 11:14:33 +0300

    add - neogit (magit)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -71,6 +71,7 @@ require('packer').startup(function()
   use 'vim-airline/vim-airline-themes'
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
+  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
   -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
