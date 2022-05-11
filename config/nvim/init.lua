687a082f7d0784dda979efd85b0d80d464623c15
iledarn <iledarn@gmail.com>
2022-05-11 16:59:36 +0300

    leader shortcut for tree

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -159,6 +159,7 @@ require('telescope').setup {
   },
 }
 --Add leader shortcuts
+vim.api.nvim_set_keymap('n', '<leader>\\', [[<cmd>CHADopen<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
