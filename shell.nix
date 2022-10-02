e1340a1d704bafaac81146e2faa8ce4d2376183a
iledarn <iledarn@gmail.com>
2022-10-02 12:49:02 +0800

    formated with nixfmt

--- shell.nix
+++ shell.nix
@@ -1,29 +1,29 @@
 { pkgs ? import <nixpkgs> {
-#  overlays = [(import (builtins.fetchTarball {url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;}))];
-}
-}:
+  overlays = [
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+    }))
+  ];
+} }:
 with pkgs;
 let
-  my-python-packages = python-packages: with python-packages; [
-    ptpython
-    # other python packages you want
-  ];
+  my-python-packages = python-packages:
+    with python-packages;
+    [
+      ptpython
+      # other python packages you want
+    ];
   python-with-my-packages = python3.withPackages my-python-packages;
 
-  neovimPythonPackages = p: with p; [
-    jedi
-    flake8
-    black
-    pylint
-  ];
+  neovimPythonPackages = p: with p; [ jedi flake8 black pylint ];
   neovim = pkgs.neovim.override {
     extraPython3Packages = neovimPythonPackages;
     withPython3 = true;
     withNodeJs = true;
   };
-in
-  mkShell {
-    buildInputs = [
+in mkShell {
+  buildInputs = [
     # Customized packages
     python-with-my-packages
     tmux
