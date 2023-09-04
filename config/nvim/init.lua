4e530e8db9f975a88b744a72664a99878f27f76e
iledarn <iledarn@gmail.com>
2023-09-04 13:24:24 +0800

    fix - not an editor command: AerialPrevUp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -187,9 +187,6 @@ require("aerial").setup({
         -- Jump forwards/backwards with '{' and '}'
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '{', '<cmd>AerialPrev<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '}', '<cmd>AerialNext<CR>', {})
-        -- Jump up the tree with '[[' or ']]'
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '[[', '<cmd>AerialPrevUp<CR>', {})
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', ']]', '<cmd>AerialNextUp<CR>', {})
     end
 })
 --Add leader shortcuts
