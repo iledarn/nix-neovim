7ac5541cf72e7daa34a7f76eb7bc8f4112b8d0e2
Ildar Nasyrov <iledarnp@gmail.com>
2024-02-27 22:00:30 +0800

    fix - NvimTree view.mappings error

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -424,23 +424,23 @@ let g:db_ui_use_nerd_fonts = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
--- require("nvim-tree").setup()
+require("nvim-tree").setup()
 
 -- OR setup with some options
-require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    view = {
-        adaptive_size = false,
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
-        },
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
-})
+-- require("nvim-tree").setup({
+--     sort_by = "case_sensitive",
+--     view = {
+--         adaptive_size = false,
+--         mappings = {
+--             list = {
+--                 { key = "u", action = "dir_up" },
+--             },
+--         },
+--     },
+--     renderer = {
+--         group_empty = true,
+--     },
+--     filters = {
+--         dotfiles = true,
+--     },
+-- })
